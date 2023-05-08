b682025b37cd560629f453a2cbe95e94b976d1cb
@bataaMid <bataa@mid.mn>
2023-05-08 12:14:22 +0800

    delgets deerees ustgahiig nemsen

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var uiController = (function() {
         tusuvLabel: ".budget__value",
         incomeLabel: ".budget__income--value",
         expenseLabel: ".budget__expenses--value",
-        percentageLabel: ".budget__expenses--percentage"
+        percentageLabel: ".budget__expenses--percentage",
+        containerDiv: ".container"
     }
     return {
         getInput: function(){
@@ -55,15 +56,20 @@ var uiController = (function() {
             }
             
         },
+
+        deleteListItem: function(id){
+            var el = document.getElementById(id);
+            el.parentNode.removeChild(el);
+        },
         addListItem: function(item, type) {
             // Orlogo zarlagiin elementiig aguulsan html beltgene
             var html, list;
             if(type === 'inc'){
                 list = DOMstrings.incomeList;
-                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             } else {
                 list = DOMstrings.expenseList;
-                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             }
             // html dotroo orlogo zarlagiin utguudiig REPLACE ashiglaj uurchilno
             html = html.replace('%id%', item.id);
@@ -130,6 +136,17 @@ var financeController = (function() {
                 huvi: data.huvi,
                 totalInc: data.totals.inc,
                 totalExp: data.totals.exp
+            }
+        },
+
+        deleteItem: function(type, id){
+            var ids = data.items[type].map(function(el){
+                return el.id;
+            })
+            var index = ids.indexOf(id);
+
+            if(index !== -1){
+                data.items[type].splice(index, 1);
             }
         },
         addItem: function(type, desc, val){
@@ -200,6 +217,24 @@ var appController = (function(uiController, financeController) {
                 ctrlAddItem();
             }
         });
+
+        document.querySelector(DOM.containerDiv).addEventListener('click', function(event){
+            var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+            if(id){
+                var arr = id.split('-');
+                var type = arr[0];
+                var itemId = parseInt(arr[1]);
+
+                console.log(type + '===>' + itemId);
+
+                //1. Sanhuugiin modulias type, id ahiglaad ustgana
+                financeController.deleteItem(type, itemId)
+                //2. Delgets deerees ustgana
+                uiController.deleteListItem(id);
+                //3. Uldegdliin tootsoog shinechilne
+            }
+            
+        })
     }
     return {
         init: function() {
